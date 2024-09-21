8c979ef1587f06d8d368d1811031be4ef8f3bb2c
playDev11 <play.dev6@gmail.com>
2024-09-21 14:17:24 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home'
+import Collection from './Pages/Collection'
+import About from './Pages/About'
+import Contact from './Pages/Contact'
+import Product from './Pages/Product'
+import Cart from './Pages/Cart'
+import Login from './Pages/Login'
+import PlaceOrder from './Pages/PlaceOrder'
+import Order from './Pages/Order'
+import Navbar from './Components/Navbar'
+
+const App = () => {
+  return (
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/collection' element={<Collection/>} />
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/product/:product_id' element={<Product/>} />
+        <Route path='/cart' element={<Cart/>} />
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/place-order' element={<PlaceOrder/>} />
+        <Route path='/order' element={<Order/>} />
+      </Routes>
+
+    </div>
+  )
+}
+
+export default App

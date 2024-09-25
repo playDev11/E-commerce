6c1c461f5f799017e06d0383d8dcdc11e100bf56
playDev11 <play.dev6@gmail.com>
2024-09-25 18:10:54 +0100

    Added onSubmitHandler to NewsLetterBox form and updated form attributes

--- src/Components/NewsLetterBox.jsx
+++ src/Components/NewsLetterBox.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
 const NewsLetterBox = () => {
+
+  const onSubmitHandler = (event)=>{
+    event.preventDefault();
+  }
   return (
     <div className='text-center'>
         <p className='text-2xl font-medium text-gray-800'>Subscribe now & Get 20% Off</p>
         <p className='text-gray-400 mt-3'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore, quis placeat.</p>
-        <form action="">
+        <form onSubmit={onSubmitHandler} className='w-full sm:w-1/2 flex items-center gap-3 mx-auto my-6 border pl-3' action="">
             <input className='w-full sm:flex-1 outline-none' type="email" placeholder='Enter Your Email' />
             <button type='submit' className='bg-black text-white text-xs px-10 py-4'>Subscribe</button>
         </form>

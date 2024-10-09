fd8592fb7096a46535f73b395691015ab0502e18
playDev11 <play.dev6@gmail.com>
2024-10-09 21:20:00 +0100

    fix: Updated Product.jsx component and assets.js file

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -8,22 +8,39 @@ const Product = () => {
   // console.log(product_id)
 const {products} = useContext(shopContext)
 const [productData, setProductData] = useState(false)
+const [image,  setImage] = useState(false)
+
 
 const fetchProductData = async () =>{
-  const products = ()=>{
-    // const response = await fetch(`https://fakestoreapi.com/products/${product_id}`)
-  }
+  products.map(item=>{
+    if (item._id === product_id) {
+      setProductData(item)
+      setImage(item.image[0])
+      return null;
+    }
+  })
 }
 useEffect(() => {
   fetchProductData();
   },[product_id])
 
   
-  return (
-    <div>
+  return productData?(
+    <div className='border-t-2 pt-10 transition-opacity ease-in duration-500 opacity-100'>
+
+
+      <div className='flex gap-12 sm:gap-12 flex-col sm:flex-row'>
+        <div className='flex-1 flex flex-col-reverse gap-3 sm:flex-row'>
+          <div className='flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[]'>
+
+          </div>
+
+        </div>
+
+      </div>
         
     </div>
-  )
+  ): <div className='opacity-0'></div>
 }
 
 export default Product
